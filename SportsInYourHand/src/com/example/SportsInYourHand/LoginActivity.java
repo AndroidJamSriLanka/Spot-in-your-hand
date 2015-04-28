92a79e2fda0317a8d00e4c25d6bc5b87520ba978
udayanga91 <u.senanayake91@gmain.com>
2015-04-28 12:34:01 +0530

    App_Interface

---
+++ SportsInYourHand/src/com/example/SportsInYourHand/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.SportsInYourHand;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+//Log in button
+        Button button =(Button)findViewById(R.id.buttonLogIn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
---
+++ SportsInYourHand/src/com/example/SportsInYourHand/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.SportsInYourHand;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Udayanga Senanayake on 4/28/2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
